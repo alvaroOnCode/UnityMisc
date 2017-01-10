42f105a21f7d31f5ea5691d6b05f2b0748b5a37b
Álvaro Saavedra <info@alvaro-oncode.com>
2017-01-10 12:06:50 +0100

    Upload the ObjectPooler script for the first time.
    Unity description: You may have made games in the past that created and destroyed a lot of items (such as bullets or enemies). What you may not have known is that the act of instantiating and destroying are inefficient and can slow your projects down. This way to optimize our project is called object pooling.

---
+++ ObjectPooler.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// - Source: https://www.raywenderlich.com/136091/object-pooling-unity
+/// 
+/// - Replace:
+/// Instantiate (obj, whatever.transform.position, whatever.transform.rotation);
+/// 
+/// - By:
+/// GameObject pooledObject = ObjectPooler.SharedInstance.GetPooledObject ();
+/// if (pooledObject != null) {
+///    pooledObject.transform.position = whatever.transform.position;
+///    pooledObject.transform.rotation = whatever.transform.rotation;
+///    pooledObject.SetActive (true);
+/// }
+/// 
+/// - If you don't need the pooledObject anymore:
+/// pooledObject.SetActive (false);
+/// 
+/// - Note that code placed on pooledObject's Start method must be moved to OnEnable. Start method is only called once after a script is enabled for the first time.
+/// </summary>
+
+public class ObjectPooler : MonoBehaviour {
+
+	//##################################################
+	//	PUBLIC REFERENCES
+	//##################################################
+	public List <GameObject> pooledObjects;
+	public GameObject objectToPool;
+	public int amountToPool;
+
+
+	//##################################################
+	//	STATIC REFERENCES
+	//##################################################
+	public static ObjectPooler SharedInstance;
+
+
+	//##################################################
+	//	UNITY FUNCTIONS
+	//##################################################
+	// Use this for initialization
+	void Start () {
+		SharedInstance = this;
+
+		pooledObjects = new List <GameObject> ();
+		for (int i = 0; i < amountToPool; i++) {
+			GameObject obj;
+
+			obj = (GameObject)Instantiate (objectToPool);
+			obj.SetActive (false); 
+			pooledObjects.Add (obj);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	//##################################################
+	//	ALVARO FUNCTIONS
+	//##################################################
+	public GameObject GetPooledObject () {
+		for (int i = 0; i < pooledObjects.Count; i++) {
+			if (!pooledObjects[i].activeInHierarchy) {
+				return pooledObjects[i];
+			}
+		}
+		return null;
+	}
+
+}
